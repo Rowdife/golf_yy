95b1c3bfba8fe97e5df4f07540be2836df535c75
Rowdife <jackpark201313@gmail.com>
2025-05-19 22:32:52 +0900

    fixed: navigate to login screen when sign out

--- lib/features/auth/views/widgets/buttons/auth_sign_out_button.dart
+++ lib/features/auth/views/widgets/buttons/auth_sign_out_button.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:golf_yy/features/auth/view_models/socialAuth.dart';
+import 'package:golf_yy/router.gr.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AuthSignOutButton extends ConsumerWidget {
@@ -7,6 +9,7 @@ class AuthSignOutButton extends ConsumerWidget {
 
   void onSignOutTap(BuildContext context, WidgetRef ref) {
     ref.read(socialAuthProvider).signOut();
+    context.router.replace(LoginRoute());
   }
 
   @override
